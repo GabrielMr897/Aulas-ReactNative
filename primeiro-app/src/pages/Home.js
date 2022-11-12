a0591b4cffdbee7219d1de89ec4a27a4d55f5c0b
Gabriel Martins <gabriel-pms@hotmail.com>
2022-11-11 21:03:37 -0300

    alterações em projeto e nova pasta

--- primeiro-app/src/pages/Home.js
+++ primeiro-app/src/pages/Home.js
@@ -1,10 +1,56 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from "react-native";
 
+import React, { useState } from 'react'
 
-export default function Home() {
+export function Home() {
+
+    const [message, setMessage] = useState()
+
     return (
-        <View>
-            <Text>Bem vindo!</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Bem vindo!</Text>
+            <TextInput style={styles.input} placeholder="Digite algo" placeholderTextColor="RGB(142, 126, 196)" onChangeText={setMessage}/>
+
+            <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+                <Text style={styles.buttonText}>Adicionar</Text>
+            </TouchableOpacity>
+
+            <Text style={[styles.title, { marginTop: 50 }]}>{message}</Text>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingHorizontal: 20,
+        paddingVertical: 60
+    },
+    title: {
+        color: '#ffffff',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#ffffff',
+        fontSize: 18,
+        padding: Platform.OS === 'android' ? 15 : 20,
+        marginTop: 20,
+        borderRadius: 7,
+    },
+    button: {
+        backgroundColor: '#ffffff',
+        padding: 12,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 15
+    },
+    buttonText: {
+        color: '#1F1E25',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
